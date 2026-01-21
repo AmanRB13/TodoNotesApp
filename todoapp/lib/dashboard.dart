7f2b1f153561f6485b2d6d54265363a3b7eb088e
Aman Ranabhat <080bct013.aman@pcampus.edu.np>
2026-01-21 19:39:54 +0545

    Update dashboard.dart

--- todoapp/lib/dashboard.dart
+++ todoapp/lib/dashboard.dart
@@ -18,9 +18,7 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: const Text('Dashboard')),
+      appBar: AppBar(title: const Text('Dashboard')),
       body: Consumer<Listprovider>(
         builder: (context, model, child) {
           if (model.alllist.isEmpty) {
@@ -28,104 +26,156 @@ class _DashboardState extends State<Dashboard> {
           }
 
           final todo = model.alllist.first;
+          final filteredindex = model.filteredIndexes;
 
-          return Padding(
-            padding: const EdgeInsets.all(14),
-            child: SingleChildScrollView(
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  spacing: 30,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.grey),
+                      ),
+                      onPressed: () {
+                        model.setfilter(0);
+                      },
+                      child: Text("All"),
+                    ),
+                    SizedBox(width: 320),
+
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.green,
+                        ),
+                      ),
+                      onPressed: () {
+                        model.setfilter(1);
+                      },
+                      child: Text("Completed"),
+                    ),
+                    SizedBox(width: 320),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.redAccent,
+                        ),
+                      ),
+                      onPressed: () {
+                        model.setfilter(2);
+                      },
+                      child: Text("Pending"),
+                    ),
+                  ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        dayName,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.all(14),
+                  child: SingleChildScrollView(
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              dayName,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+                            Text(
+                              date,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: filteredindex.length,
+                              itemBuilder: (context, i) {
+                                final realIndex = filteredindex[i];
+                                return Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    vertical: 6,
+                                  ),
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          todo.title[realIndex],
+                                          style: const TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                        time,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+
+                                      GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            todo.isdone[realIndex] =
+                                                !todo.isdone[realIndex];
+                                          });
+                                        },
+                                        child: Icon(
+                                          todo.isdone[realIndex]
+                                              ? Icons.check_box
+                                              : Icons.check_box_outline_blank,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+
+                                      const SizedBox(width: 10),
+
+                                      TextButton(
+                                        onPressed: () {
+                                          // Call the removeTodo method from provider
+                                          model.removeTodo(realIndex);
+                                        },
+                                        child: const Icon(Icons.delete),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                      Text(
-                        date,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-
-                      ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: todo.title.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: const EdgeInsets.symmetric(vertical: 6),
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.blueGrey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    todo.title[index],
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Text(
-                                  time,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      todo.isdone[index] = !todo.isdone[index];
-                                    });
-                                  },
-                                  child: Icon(
-                                    todo.isdone[index]
-                                        ? Icons.check_box
-                                        : Icons.check_box_outline_blank,
-                                    color: Colors.white,
-                                  ),
-                                ),
-
-                                const SizedBox(width: 10),
-
-                                TextButton(
-                                  onPressed: () {
-                                    // Call the removeTodo method from provider
-                                    model.removeTodo(index);
-                                  },
-                                  child: const Icon(Icons.delete),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
           );
         },
